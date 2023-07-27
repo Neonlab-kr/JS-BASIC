844f17b1d09fafed5be4ffdde7f0b2d6bbfdc508
Neonlab_kr <ksknk67@gmail.com>
2023-07-27 16:19:09 +0900

    section 8,9

--- ToDo-list/script.js
+++ ToDo-list/script.js
@@ -1,10 +1,11 @@
-const todoList = document.getElementById("todo-list");
-const inputValue = document.getElementById("todo-input");
+const todoInput = document.querySelector("#todo-input");
+const todoList = document.querySelector("#todo-list");
 
+const savedWeatherData = JSON.parse(localStorage.getItem("saved-weather"));
 const savedTodoList = JSON.parse(localStorage.getItem("saved-items"));
 
-const createTodo = (storageData) => {
-  let todoContents = inputValue.value;
+const createTodo = function (storageData) {
+  let todoContents = todoInput.value;
   if (storageData) {
     todoContents = storageData.contents;
   }
@@ -31,17 +32,17 @@ const createTodo = (storageData) => {
   newLi.appendChild(newBtn);
   newLi.appendChild(newSpan);
   todoList.appendChild(newLi);
-  inputValue.value = "";
+  todoInput.value = "";
   saveItemsFn();
 };
 
-const keyCodeCheck = () => {
-  if (window.event.keyCode === 13 && inputValue.value != "") {
+const keyCodeCheck = function () {
+  if (window.event.keyCode === 13 && todoInput.value.trim() !== "") {
     createTodo();
   }
 };
 
-const deleteAll = () => {
+const deleteAll = function () {
   const liList = document.querySelectorAll("li");
   for (let i = 0; i < liList.length; i++) {
     liList[i].remove();
@@ -49,7 +50,7 @@ const deleteAll = () => {
   saveItemsFn();
 };
 
-const saveItemsFn = () => {
+const saveItemsFn = function () {
   const saveItems = [];
   for (let i = 0; i < todoList.children.length; i++) {
     const todoObj = {
@@ -58,6 +59,7 @@ const saveItemsFn = () => {
     };
     saveItems.push(todoObj);
   }
+
   saveItems.length === 0
     ? localStorage.removeItem("saved-items")
     : localStorage.setItem("saved-items", JSON.stringify(saveItems));
@@ -67,4 +69,73 @@ if (savedTodoList) {
   for (let i = 0; i < savedTodoList.length; i++) {
     createTodo(savedTodoList[i]);
   }
+}
+
+const weatherDataActive = function ({ location, weather }) {
+  const weatherMainList = [
+    "Clear",
+    "Clouds",
+    "Drizzle",
+    "Rain",
+    "Snow",
+    "Thunderstorm",
+  ];
+  weather = weatherMainList.includes(weather) ? weather : "Fog";
+  const locationNameTag = document.querySelector("#location-name-tag");
+
+  locationNameTag.textContent = location;
+  document.body.style.backgroundImage = `url('./images/${weather}.jpg')`;
+
+  if (
+    !savedWeatherData ||
+    savedWeatherData?.location !== location ||
+    savedWeatherData?.weather !== weather
+  ) {
+    localStorage.setItem(
+      "saved-weather",
+      JSON.stringify({ location, weather })
+    );
+  }
+};
+
+const weatherSearch = function ({ latitude, longitude }) {
+  fetch(
+    `https://api.openweathermap.org/data/3.0/onecall?lat=${latitude}&lon=${longitude}&appid=87fb2cd1cb4bc07a4c69ed0dc179e989`
+  )
+    .then((res) => {
+      return res.json();
+    })
+    .then((json) => {
+      console.log(json);
+      const weatherData = {
+        location: json.name,
+        weather: json.current.weather[0].main,
+      };
+      weatherDataActive(weatherData);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+const accessToGeo = function ({ coords }) {
+  const { latitude, longitude } = coords;
+  // shorthand property
+  const positionObj = {
+    latitude,
+    longitude,
+  };
+
+  weatherSearch(positionObj);
+};
+
+const askForLocation = function () {
+  navigator.geolocation.getCurrentPosition(accessToGeo, (err) => {
+    console.log(accessToGeo);
+    console.log(err);
+  });
+};
+askForLocation();
+if (savedWeatherData) {
+  weatherDataActive(savedWeatherData);
 }
